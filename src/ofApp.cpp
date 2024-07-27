f62373a5a775fa95cbe598dd469a559f27e843a3
hiroka <mementoka@gmail.com>
2024-07-27 20:17:19 +0900

    add initial scenes

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,13 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
   ofBackground(0);
-  ofSetColor(255);
+  ofSetColor(255, 80, 10);
+  futura.load("Futura.ttc", 84);
+  optima.load("Optima.ttc", 84);
+  for (int i = 0; i < 8; i++) {
+    HMD_IMAGES[i].load("img/" + ofToString(i) + ".jpeg");
+  }
+  logo.load("img/logo.png");
 
 }
 
@@ -14,12 +20,38 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+  ofBackground(0);
+  int frameCount = ofGetFrameNum();
+  float fontRed = abs(sin(frameCount * 0.01)) * 255;
   if (scene == 0) {
-    ofDrawBitmapString("HMD SHOW BY TSUKAMOTO TERADA LAB", ofGetHeight()/2, ofGetWidth()/2);
+    float titleY = ofGetWidth()/2-120;
+    float yPad = 120;
+    ofSetColor(255, 0, 0);
+    //logo.draw(ofGetWidth()/2+100, 80, 580, 580);
+    ofSetColor(fontRed, 0, 0);
+    optima.drawString("HMD SHOW", 0, titleY);
+    optima.drawString("BY", 0, titleY+yPad);
+    optima.drawString("TSUKAMOTO TERADA LAB.", 0, titleY+2*yPad);
   } else if (scene == 1) {
-    for (int i = 0; i < 10; i++) {
-      ofDrawBitmapString(HMD_NAMES[i], 10, 10 + i * 20);
-    }
+    ofSetColor(255, 0, 0);
+    float makerX = ofGetWidth();
+    float makerY = 120;
+    nameTextBB = futura.getStringBoundingBox(HMD_NAMES[hmdNum], makerX, makerY);
+    float nameX = ofGetWidth()/2;
+    float nameY = ofGetHeight()/2+300;
+    makerTextBB = futura.getStringBoundingBox(HMD_MAKERS[hmdNum], nameX, nameY);
+
+
+    optima.drawString(HMD_MAKERS[hmdNum], makerX-makerTextBB.width-20, makerY);
+    //ofDrawRectangle(makerX, makerY-makerTextBB.height, makerTextBB.width, makerTextBB.height);
+
+    ofNoFill();
+    ofSetLineWidth(4);
+    ofDrawRectangle(ofGetWidth()/2-340, 220, 800, 580);
+    ofSetColor(255);
+    HMD_IMAGES[hmdNum].draw(ofGetWidth()/2-400, 160, 780, 560);
+    ofSetColor(240, 0, 0);
+    futura.drawString(HMD_NAMES[hmdNum], nameX-nameTextBB.width/2, nameY);
   }
 }
 
@@ -31,6 +63,13 @@ void ofApp::keyPressed(int key){
       break;
     case '1':
       scene = 1;
+      break;
+    case OF_KEY_LEFT:
+      hmdNum = (hmdNum - 1) % 8;
+      ofLog() << makerTextBB;
+      break;
+    case OF_KEY_RIGHT:
+      hmdNum = (hmdNum + 1) % 8;
       break;
     default:
       break;
--- src/ofApp.h
+++ src/ofApp.h
@@ -20,11 +20,20 @@ class ofApp : public ofBaseApp{
 		//void dragEvent(ofDragInfo dragInfo);
 		//void gotMessage(ofMessage msg);
 
-    ofTrueTypeFont myFont;
+    ofTrueTypeFont futura;
+    ofTrueTypeFont optima;
     //string array of names
-    string HMD_NAMES[10] = {"Apple Vision Pro", "Meta Quest 3", "Xreal light", "Xreal Air 2 Ultra",
-                        "Vive Pro", "Hololens 2", "Moverio BT40S", "Vuzix M4000", "Vuzix z100",
-                        "Monocle"};
+    string HMD_NAMES[8] = {"Vuzix z100", "Meta Quest 3", "Vuzix M4000", "Xreal Air 2",
+      "Apple Vision Pro", "Moverio BT-40S", "Hololens 2", "Monocle"};
+    string HMD_MAKERS[8] = {"Vuzix", "Meta", "Vuzix", "Xreal",
+      "Apple", "Epson", "Microsoft", "Brilliant Labs"};
     int scene = 0;
+    int hmdNum = 0;
+
+    ofRectangle makerTextBB;
+    ofRectangle nameTextBB;
+
+    ofImage HMD_IMAGES[8];
+    ofImage logo;
 
 };
