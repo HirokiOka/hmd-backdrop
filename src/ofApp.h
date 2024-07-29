99635ab9ee8437969b113cded1322222be145ad6
hiroka <mementoka@gmail.com>
2024-07-29 13:13:51 +0900

    fix typo

--- src/ofApp.h
+++ src/ofApp.h
@@ -24,8 +24,8 @@ class ofApp : public ofBaseApp{
     ofTrueTypeFont optima;
     ofTrueTypeFont pageFont;
     //string array of names
-    string HMD_NAMES[8] = {"Vuzix z100", "Meta Quest 3", "Vuzix M4000", "Xreal Air 2",
-      "Apple Vision Pro", "Moverio BT-40S", "Hololens 2", "Monocle"};
+    string HMD_NAMES[8] = {"Vuzix Z100", "Meta Quest 3", "Vuzix M4000", "Xreal Air 2",
+      "Apple Vision Pro", "MOVERIO BT-40S", "HoloLens 2", "Monocle"};
     string HMD_MAKERS[8] = {"Vuzix", "Meta", "Vuzix", "Xreal",
       "Apple", "Epson", "Microsoft", "Brilliant Labs"};
     int scene = 0;
